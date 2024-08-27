1fbd601cc418800b66b93947c226246bf2017d6b
Arold Mauger <aroldmauger2022@gmail.com>
2024-08-27 19:19:25 +0200

    Ajout de CI

---
+++ src/SimpleReactSelector.test.tsx
@@ -0,0 +1,13 @@
+import SimpleReactSelector from "./SimpleReactSelector";
+/*
+describe("component", () => {
+    test("toto", () => {
+        expect(1).toBe(1)
+    })
+})
+
+*/
+// avec JEST
+// voir que le trigger click fonctionne bien
+// qu'il soit ouvert ou pas
+// qu'une option du sélecteur soit bien présente ou pas dans le DOM
